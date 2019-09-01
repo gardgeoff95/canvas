6430ac1480b71e463b51596ead5c00e87ec8e0f7
Geoff <gardgeoff95@email.arizona.edu>
2019-09-01 00:18:48 -0700

    up/down still bugged

--- public/js/grid.js
+++ public/js/grid.js
@@ -5,26 +5,22 @@ const tileHeight = 32;
 const bTiles = new Image();
 const charSprite = new Image();
 
-let frameCount = 0
+let frameLimit = 3;
+let frameCount = 0;
 let directionIdle = 0;
-let directionUp = 1;
-let directionDown = 2;
-let directionRight = 3;
-let directionLeft = 4;
+let directionLeft = 1;
+let directionRight = 2;
+let directionDown = 3;
+let directionUp = 4;
 
 let character = {
-  moveLoopIndex : 0,
-  idleLoop : [0, 1, 2, 3, 4, 5, 6, 7],
-  direction : directionIdle,
+  moveLoopIndex: 0,
+  animationLoop: [0, 1, 2, 3],
+  direction: directionRight,
   row: 5,
   col: 5,
-  speed: 2,
-
-
-
-}
- 
-
+  speed: 2
+};
 
 let maps = {
   mapOne: [
@@ -44,15 +40,24 @@ let maps = {
 canvas.width = tileWidth * maps.mapOne[0].length;
 canvas.height = tileHeight * maps.mapOne.length;
 function loadImage() {
-  bTiles.src = "/images/woodTileset.png";
+  bTiles.src = "/images/woodTilesetGrid.png";
   bTiles.onload = () => {
     window.requestAnimationFrame(gameLoop);
   };
-  charSprite.src = "/images/hatidle.png";
+  charSprite.src = "/images/character.png";
   bTiles.onload = () => {
     window.requestAnimationFrame(gameLoop);
-  }
+  };
+}
 
+let keyPresses = {};
+window.addEventListener("keydown", keyDownListener, false);
+function keyDownListener(event) {
+  keyPresses[event.key] = true;
+}
+window.addEventListener("keyup", keyUpListener, false);
+function keyUpListener(event) {
+  keyPresses[event.key] = false;
 }
 loadImage();
 //0 = background 1 = corner 2 = edge
@@ -70,9 +75,18 @@ function draw(sx, sy, x, y) {
     32
   );
 }
-function drawChar(image, frameX, frameY, cx, cy){
-  ctx.drawImage(image, frameX * tileWidth, frameY * tileHeight, tileWidth, tileHeight, cx * tileWidth, cy * tileHeight, tileWidth, tileHeight);
-
+function drawChar(image, frameX, frameY, cx, cy) {
+  ctx.drawImage(
+    image,
+    frameX * tileWidth,
+    frameY * tileHeight,
+    tileWidth,
+    tileHeight,
+    cx * tileWidth,
+    cy * tileHeight,
+    tileWidth,
+    tileHeight
+  );
 }
 
 function renderStage() {
@@ -115,23 +129,96 @@ function renderStage() {
     }
   }
 }
-
+console.log
 function gameLoop() {
-  frameCount ++;
-  if (frameCount < 5) {
+  frameCount++;
+  if (frameCount < frameLimit) {
     window.requestAnimationFrame(gameLoop);
     return;
   }
-  frameCount = 0
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  frameCount = 0;
 
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   renderStage();
+
   
-  drawChar(charSprite, character.idleLoop[character.moveLoopIndex], 0, character.row, character.col);
-  character.moveLoopIndex ++;
-  if (character.moveLoopIndex >= character.idleLoop.length) {
-    character.moveLoopIndex = 0
+
+
+  let hasMoved = false;
+  if (keyPresses.w) {
+
+    character.direction = directionUp;
+    character.animationLoop = [0, 1, 2, 3];
+    hasMoved = true;
+    moveCharacter("up")
+  } else if (keyPresses.s) {
+  
+    character.direction = directionDown;
+    character.animationLoop = [0, 1, 2, 3];
+    hasMoved = true;
+    moveCharacter("down")
+  } else if (keyPresses.a) {
+ 
+    character.direction = directionLeft;
+    character.animationLoop = [0, 1, 2, 3];
+    hasMoved = true;
+    moveCharacter("left")
+
+  } else if (keyPresses.d) {
+   
+    character.direction = directionRight;
+    character.animationLoop = [0, 1, 2, 3];
+    hasMoved = true;
+    moveCharacter("right")
+  }
+
+  if (hasMoved) {
+    
+    frameCount++;
+    if (frameCount >= frameLimit) {
+      frameCount = 0;
+      character.moveLoopIndex++;
+      if (character.moveLoopIndex >= character.animationLoop.length) {
+        character.moveLoopIndex = 0;
+      }
+    }
+  }
+
+  if (!hasMoved) {
+    
+    character.direction = 0;
+    character.animationLoop = [0, 1, 2, 3, 4, 5, 6, 7];
+  }
+  drawChar(
+    charSprite,
+    character.animationLoop[character.moveLoopIndex],
+    character.direction,
+    character.row,
+    character.col
+  );
+  character.moveLoopIndex++;
+  if (character.moveLoopIndex >= character.animationLoop.length) {
+    character.moveLoopIndex = 0;
   }
 
   window.requestAnimationFrame(gameLoop);
+}
+function moveCharacter(direction){
+
+ 
+
+  if (direction === "left" && maps.mapOne[character.col][character.row -1] <=8) {
+    character.row -= 1;
+  } else if (direction === "right" && maps.mapOne[character.col][character.row +1] <= 8) {
+    character.row += 1;
+  } else if (direction === "down" && maps.mapOne[character.col][character.row] <=8) {
+    character.col +=1;
+   } else if (direction === "up" && maps.mapOne[character.col][character.row] <= 8) {
+    console.log("hello?")
+    character.col -=1;
+  }
+ 
+
+
+
 }
